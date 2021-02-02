1b64b7e5c1848b46f01478dfe1bf9f310dc43f8f
Dan <14043624+delivrance@users.noreply.github.com>
2021-02-02 11:15:05 +0100

    Update commands.py

--- assistant/plugins/commands.py
+++ assistant/plugins/commands.py
@@ -360,11 +360,11 @@ async def fmt(_, message: Message):
 
 ################################	
 
-DEV = (	
-    "The fix for this issue has already been pushed to the `master` branch on "	
-    "[GitHub](https://github.com/pyrogram/pyrogram). You can now upgrade Pyrogram with:\n\n"	
-    "`pip3 install -U https://github.com/pyrogram/pyrogram/archive/master.zip`"	
-)	
+DEV = (
+    "The fix for this issue has already been pushed to the `master` branch on "
+    "[GitHub](https://github.com/pyrogram/pyrogram). You can now upgrade Pyrogram with:\n\n"
+    "`pip3 install -U https://github.com/pyrogram/pyrogram/archive/master.zip`"
+)
 
 
 @Assistant.on_message(command("dev"))
